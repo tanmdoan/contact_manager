565bf9f66dd9eb46feaf1dc7cb2a0b52cb525fcf
Tan Doan <tdoan87@gmail.com>
2014-08-06 07:59:28 -0600

    set valid attributes for email addresses

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe EmailAddress, :type => :model do
-  let(:email_address) {EmailAddress.new(address: "hi@example.com")}
+  let(:email_address) { EmailAddress.new(address: "hi@example.com", person_id: 1 ) }
 
   it 'is valid' do
     expect(email_address).to be_valid
@@ -9,6 +9,11 @@ RSpec.describe EmailAddress, :type => :model do
 
   it 'is not valid without an address' do
     email_address.address = nil
+    expect(email_address).not_to be_valid
+  end
+
+  it 'is not valid without a person id' do
+    email_address.person_id = nil
     expect(email_address).not_to be_valid
   end
 end
