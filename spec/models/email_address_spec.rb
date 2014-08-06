c0c8927c8a936677db3a23efd4a45e962c03778a
Tan Doan <tdoan87@gmail.com>
2014-08-05 22:47:18 -0600

    email address validation complete

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe EmailAddress, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:email_address) {EmailAddress.new(address: "hi@example.com")}
+
+  it 'is valid' do
+    expect(email_address).to be_valid
+  end
+
+  it 'is not valid without an address' do
+    email_address.address = nil
+    expect(email_address).not_to be_valid
+  end
 end
